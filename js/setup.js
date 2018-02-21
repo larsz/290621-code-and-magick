5514f127fe4d4c456eaeb27bb14d44f5758a3cbd
larsz <eugen.lobicov@gmail.com>
2018-02-21 11:39:20 +0200

    Перетаскивает попап и предметы волшебника

--- js/setup.js
+++ js/setup.js
@@ -57,4 +57,66 @@
   showSimilarWizards(generatedWizards);
 
   document.querySelector('.setup-similar').classList.remove('hidden');
+
+  var shopElement = document.querySelector('.setup-artifacts-shop');
+  var artifactsElement = document.querySelector('.setup-artifacts');
+  var artifactCellElement = artifactsElement.querySelectorAll('.setup-artifacts-cell');
+  var draggedItem = null;
+
+  shopElement.addEventListener('dragstart', function (evt) {
+    if (evt.target.tagName.toLowerCase() === 'img') {
+      draggedItem = evt.target;
+      evt.dataTransfer.setData('text/plain', evt.target.alt);
+    }
+
+    artifactCellElement.forEach(function (cell) {
+      cell.style.outline = '2px dashed red';
+    });
+  });
+
+  artifactsElement.addEventListener('dragover', function (evt) {
+    evt.preventDefault();
+    return false;
+  });
+
+  artifactsElement.addEventListener('dragstart', function () {
+    artifactCellElement.forEach(function (cell) {
+      cell.style.outline = '2px dashed red';
+    });
+  });
+
+  artifactsElement.addEventListener('drop', function (evt) {
+    evt.preventDefault();
+    evt.target.style.backgroundColor = '';
+
+    if (evt.target.tagName.toLowerCase() === 'div') {
+      evt.target.appendChild(draggedItem);
+    }
+
+    artifactCellElement.forEach(function (cell) {
+      cell.style.outline = '';
+    });
+  });
+
+  artifactsElement.addEventListener('dragenter', function (evt) {
+    evt.preventDefault();
+    if (evt.target.tagName.toLowerCase() === 'div') {
+      evt.target.style.backgroundColor = 'yellow';
+    }
+  });
+
+  artifactsElement.addEventListener('dragleave', function (evt) {
+    evt.preventDefault();
+    evt.target.backgroundColor = '';
+  });
+
+  document.addEventListener('dragend', function (evt) {
+    evt.preventDefault();
+
+    artifactCellElement.forEach(function (cell) {
+      cell.style.backgroundColor = '';
+    });
+
+  });
+
 })();
