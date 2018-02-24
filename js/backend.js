0990af6dfd805089b0216cf8cc8be0e5d5c3720d
larsz <eugen.lobicov@gmail.com>
2018-02-24 23:52:36 +0200

    Исправляет замечания

--- js/backend.js
+++ js/backend.js
@@ -1,16 +1,42 @@
 'use strict';
 
 (function () {
-  var load = function (onLoad, onError) {
+
+  var SAVE_URL = 'https://js.dump.academy/code-and-magick';
+  var LOAD_URL = 'https://js.dump.academy/code-and-magick/data/';
+
+  var Code = {
+    SUCCESS: 200,
+    BAD_REQUEST: 400,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    SERVER_ERROR: 500
+  };
+
+  var initXHR = function (onLoad, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
-    xhr.timeout = 1000; // 10s
+    xhr.timeout = 10000; // 10s
 
     xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onLoad(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      switch (xhr.status) {
+        case Code.SUCCESS:
+          onLoad(xhr.response);
+          break;
+        case Code.BAD_REQUEST:
+          onError('Статус ответа: ' + xhr.status + '. В запросе синтаксическая ошибка.');
+          break;
+        case Code.FORBIDDEN:
+          onError('Статус ответа: ' + xhr.status + '. В запросе отказано, недостаточно прав.');
+          break;
+        case Code.NOT_FOUND:
+          onError('Статус ответа: ' + xhr.status + '. Страница не найдена');
+          break;
+        case Code.SERVER_ERROR:
+          onError('Статус ответа: ' + xhr.status + '. Ой, неполадки на сервере, попробуйте чуть позже.');
+          break;
+        default:
+          onError('Статус ответа: ' + xhr.status + '' + xhr.statusText);
       }
     });
 
@@ -22,21 +48,18 @@
       onError('Запрос не успел выполниться за ' + xhr.timeout + ' мс');
     });
 
-    xhr.open('GET', 'https://js.dump.academy/code-and-magick/data');
+    return xhr;
+  };
+
+  var load = function (onLoad, onError) {
+    var xhr = initXHR(onLoad, onError);
+    xhr.open('GET', LOAD_URL);
     xhr.send();
   };
+
   var save = function (data, onLoad, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.addEventListener('load', function () {
-      onLoad(xhr.response);
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Ошибка соединения');
-    });
-
-    xhr.open('POST', 'https://js.dump.academy/code-and-magick');
+    var xhr = initXHR(onLoad, onError);
+    xhr.open('POST', SAVE_URL);
     xhr.send(data);
   };
 
